9b2a10de5af24235f8c548dd1d74b67cb4c63619
Ruan nunes oliveira <oliveiraruan199@gmail.com>
2026-01-27 10:06:09 -0300

    feat: add schema.sql in banco de dados/projeto apenas com SQL/Codigos_SQL

---
+++ Banco de Dados/Projeto apenas com SQL/Codigos_SQL/schema.sql
@@ -0,0 +1,51 @@
+PRAGMA foreign_keys = ON;
+
+-- USUARIOS
+CREATE TABLE IF NOT EXISTS usuarios(
+    usuario_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    nome TEXT NOT NULL,
+    idade INTEGER DEFAULT 0
+);
+
+-- MATÉRIAS
+CREATE TABLE IF NOT EXISTS materias(
+    id_materia INTEGER PRIMARY KEY AUTOINCREMENT,
+    materia TEXT NOT NULL UNIQUE
+);
+
+-- TOPICOS
+CREATE TABLE IF NOT EXISTS topicos(
+    id_topico INTEGER PRIMARY KEY AUTOINCREMENT,
+    topico TEXT NOT NULL UNIQUE 
+);
+
+-- MATERIAS COM SEUS TOPICOS
+CREATE TABLE IF NOT EXISTS materia_topicos(
+    materia_id INTEGER NOT NULL,
+    topico_id INTEGER NOT NULL,
+    PRIMARY KEY(materia_id, topico_id),
+    FOREIGN KEY(materia_id) REFERENCES materias(id_materia),
+    FOREIGN KEY(topico_id) REFERENCES topicos(id_topico)
+);
+
+-- PROVAS
+CREATE TABLE IF NOT EXISTS provas(
+    prova_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    usuario_id INTEGER NOT NULL,
+    materia_id INTEGER NOT NULL,
+    data_da_prova DATE NOT NULL CHECK(data_da_prova >= '2026/01/01'),
+    resultado REAL NOT NULL,
+    FOREIGN KEY(materia_id) REFERENCES materias(id_materia),
+    FOREIGN KEY(usuario_id) REFERENCES usuarios(usuario_id)
+);
+
+-- ESTUDOS
+CREATE TABLE IF NOT EXISTS estudos(
+    id_estudo INTEGER PRIMARY KEY AUTOINCREMENT,
+    usuario_id INTEGER NOT NULL,
+    topico_estudado INTEGER NOT NULL,
+    tempo_de_estudo TIME NOT NULL CHECK(tempo_de_estudo > '00:00:00'),
+    data_do_estudo DATE NOT NULL CHECK(data_do_estudo >= '2026/01/01'),
+    FOREIGN KEY(usuario_id) REFERENCES usuarios(usuario_id),
+    FOREIGN KEY(topico_estudado) REFERENCES topicos(id_topico)
+);
