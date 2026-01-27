5cb18b5d7e7144cf39ff4723f8c433ad04bcf4b5
Ruan nunes oliveira <oliveiraruan199@gmail.com>
2026-01-27 14:05:41 -0300

    fix: banco de dados/schema

--- Banco de Dados/Projeto apenas com SQL/Codigos_SQL/schema.sql
+++ Banco de Dados/Projeto apenas com SQL/Codigos_SQL/schema.sql
@@ -1,10 +1,10 @@
 PRAGMA foreign_keys = ON;
 
--- USUARIOS
+-- USUÁRIOS
 CREATE TABLE IF NOT EXISTS usuarios(
     usuario_id INTEGER PRIMARY KEY AUTOINCREMENT,
     nome TEXT NOT NULL,
-    idade INTEGER DEFAULT 0
+    idade INTEGER
 );
 
 -- MATÉRIAS
@@ -13,13 +13,13 @@ CREATE TABLE IF NOT EXISTS materias(
     materia TEXT NOT NULL UNIQUE
 );
 
--- TOPICOS
+-- TÓPICOS
 CREATE TABLE IF NOT EXISTS topicos(
     id_topico INTEGER PRIMARY KEY AUTOINCREMENT,
     topico TEXT NOT NULL UNIQUE 
 );
 
--- MATERIAS COM SEUS TOPICOS
+-- MATERIAS COM SEUS TÓPICOS
 CREATE TABLE IF NOT EXISTS materia_topicos(
     materia_id INTEGER NOT NULL,
     topico_id INTEGER NOT NULL,
@@ -32,10 +32,10 @@ CREATE TABLE IF NOT EXISTS materia_topicos(
 CREATE TABLE IF NOT EXISTS provas(
     prova_id INTEGER PRIMARY KEY AUTOINCREMENT,
     usuario_id INTEGER NOT NULL,
-    materia_id INTEGER NOT NULL,
-    data_da_prova DATE NOT NULL CHECK(data_da_prova >= '2026/01/01'),
-    resultado REAL NOT NULL,
-    FOREIGN KEY(materia_id) REFERENCES materias(id_materia),
+    materia_da_prova_id INTEGER NOT NULL,
+    data_da_prova DATE NOT NULL CHECK(data_da_prova >= '2026-01-01'),
+    resultado REAL NOT NULL CHECK(resultado >= 0 AND resultado <= 10),
+    FOREIGN KEY(materia_da_prova_id) REFERENCES materias(id_materia),
     FOREIGN KEY(usuario_id) REFERENCES usuarios(usuario_id)
 );
 
@@ -44,8 +44,8 @@ CREATE TABLE IF NOT EXISTS estudos(
     id_estudo INTEGER PRIMARY KEY AUTOINCREMENT,
     usuario_id INTEGER NOT NULL,
     topico_estudado INTEGER NOT NULL,
-    tempo_de_estudo TIME NOT NULL CHECK(tempo_de_estudo > '00:00:00'),
-    data_do_estudo DATE NOT NULL CHECK(data_do_estudo >= '2026/01/01'),
+    tempo_de_estudo TIME NOT NULL CHECK(tempo_de_estudo >= '00:10:00'),
+    data_do_estudo DATE NOT NULL CHECK(data_do_estudo >= '2026-01-01'),
     FOREIGN KEY(usuario_id) REFERENCES usuarios(usuario_id),
     FOREIGN KEY(topico_estudado) REFERENCES topicos(id_topico)
 );
