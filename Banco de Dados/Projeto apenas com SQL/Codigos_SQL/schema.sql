f18c2e158efbf4821035355df0fe740eb65480a9
Ruan nunes oliveira <oliveiraruan199@gmail.com>
2026-01-31 15:34:34 -0300

    fix: SCHEMA - TABLE ESTUDOS (tempo_de_estudo) redone

--- Banco de Dados/Projeto apenas com SQL/Codigos_SQL/schema.sql
+++ Banco de Dados/Projeto apenas com SQL/Codigos_SQL/schema.sql
@@ -16,16 +16,13 @@ CREATE TABLE IF NOT EXISTS materias(
 -- TÓPICOS
 CREATE TABLE IF NOT EXISTS topicos(
     topico_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    materia_id INTEGER,
-    topico TEXT NOT NULL UNIQUE,
-    FOREIGN KEY(materia_id) REFERENCES materias(materia_id)
+    topico TEXT NOT NULL UNIQUE
 );
 
 -- MATERIAS COM SEUS TÓPICOS
 CREATE TABLE IF NOT EXISTS materia_topicos(
     materia_id INTEGER NOT NULL,
     topico_id INTEGER NOT NULL,
-    PRIMARY KEY(materia_id, topico_id),
     FOREIGN KEY(materia_id) REFERENCES materias(materia_id),
     FOREIGN KEY(topico_id) REFERENCES topicos(topico_id)
 );
@@ -35,7 +32,7 @@ CREATE TABLE IF NOT EXISTS provas(
     prova_id INTEGER PRIMARY KEY AUTOINCREMENT,
     usuario_id INTEGER NOT NULL,
     materia_da_prova_id INTEGER NOT NULL,
-    data_da_prova DATE NOT NULL CHECK(data_da_prova >= '2026-01-01'),
+    data_da_prova INTEGER NOT NULL CHECK(data_da_prova >= '2026-01-01'),
     resultado REAL NOT NULL CHECK(resultado >= 0 AND resultado <= 10),
     FOREIGN KEY(materia_da_prova_id) REFERENCES materias(materia_id),
     FOREIGN KEY(usuario_id) REFERENCES usuarios(usuario_id)
@@ -46,8 +43,8 @@ CREATE TABLE IF NOT EXISTS estudos(
     id_estudo INTEGER PRIMARY KEY AUTOINCREMENT,
     usuario_id INTEGER NOT NULL,
     topico_estudado INTEGER NOT NULL,
-    tempo_de_estudo TIME NOT NULL CHECK(tempo_de_estudo >= '00:10:00'),
-    data_do_estudo DATE NOT NULL CHECK(data_do_estudo >= '2026-01-01'),
+    tempo_de_estudo INTEGER NOT NULL CHECK (tempo_de_estudo >= 600),
+    data_do_estudo TEXT NOT NULL CHECK(data_do_estudo >= '2026-01-01'),
     FOREIGN KEY(usuario_id) REFERENCES usuarios(usuario_id),
     FOREIGN KEY(topico_estudado) REFERENCES topicos(topico_id)
 );
